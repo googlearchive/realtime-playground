cd7e52fa682cd72db4f4b9c2732f02465c65986e
Seth Howard <sethhoward@google.com>
2014-06-24 14:56:37 -0600

    removed local data

--- js/rtpg.js
+++ js/rtpg.js
@@ -28,10 +28,10 @@
 var rtpg = rtpg || {};
 
 /** Your Application ID from the Google APIs Console. */
-rtpg.APP_ID = '840867953062';
+rtpg.APP_ID = 'YOUR APP ID';
 
 /** Your application's Client ID from the Google APIs Console. */
-rtpg.CLIENT_ID = '840867953062.apps.googleusercontent.com';
+rtpg.CLIENT_ID = 'YOUR CLIENT ID';
 
 rtpg.realtimeDoc = null;
 
